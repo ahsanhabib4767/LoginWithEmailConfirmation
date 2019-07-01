3c6a36ddc3bea330722807927e8e1e0d7fd9865b
Your Name <rabbysah8@gmail.com>
2019-07-01 16:50:39 +0600

    first commit

---
+++ userReg/Controllers/RegisterController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using userReg.Models;
+using System.IO;
+using System.Web.Hosting;
+using System.Text;
+using System.Net.Mail;
+
+namespace userReg.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        EmpRegEntities1 db = new EmpRegEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult SaveData(SiteUser model) {
+            db.SiteUsers.Add(model);
+            db.SaveChanges();
+
+            BuildEmailTemplate(model.Id);
+            return Json("Registration successfull",JsonRequestBehavior.AllowGet);
+
+
+        }
+        public ActionResult Confirm(int regId)
+        {
+            ViewBag.regID = regId;
+            return View();
+        }
+        public JsonResult RegisterConfirm(int regId)
+        {
+            SiteUser Data = db.SiteUsers.Where(x => x.Id == regId).FirstOrDefault();
+            Data.IsValid = true;
+            db.SaveChanges();
+            var msg = "Your Email Is Verified!";
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+        public void BuildEmailTemplate(int regID)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/") + "Text" + ".cshtml");
+            var regInfo = db.SiteUsers.Where(x => x.Id == regID).FirstOrDefault();
+            var url = "http://localhost:64276/" + "Register/Confirm?regId=" + regID;
+            body = body.Replace("@ViewBag.ConfirmationLink", url);
+            body = body.ToString();
+            BuildEmailTemplate("Your Account Is Successfully Created", body, regInfo.Email);
+        }
+
+        public static void BuildEmailTemplate(string subjectText, string bodyText, string sendTo)
+        {
+            string from, to, bcc, cc, subject, body;
+            from = "******@gmail.com";
+            to = sendTo.Trim();
+            bcc = "";
+            cc = "";
+            subject = subjectText;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(bodyText);
+            body = sb.ToString();
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            mail.To.Add(new MailAddress(to));
+            if (!string.IsNullOrEmpty(bcc))
+            {
+                mail.Bcc.Add(new MailAddress(bcc));
+            }
+            if (!string.IsNullOrEmpty(cc))
+            {
+                mail.CC.Add(new MailAddress(cc));
+            }
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SendEmail(mail);
+        }
+
+        public static void SendEmail(MailMessage mail)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("*******@gmail.com", "***pass**");
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
